9de24afbfa1e5886da559fdbf35eea98668413f9
sahilcool-nsut <schawla516@gmail.com>
2022-08-21 08:09:10 +0530

    small change

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import os
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Home Page"
+
 @app.route('/helloWorld', methods=['GET'])
 def helloWorld():
     if request.method == 'GET':
