68790ea7a98d2ed243bca10064d8a16404963d03
sahilcool-nsut <schawla516@gmail.com>
2022-11-12 10:40:10 +0530

    website almost completed

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from myWeb3 import CustomWeb3
 import json
 import bcrypt
 from mongoDBDriver import insertOne
+import hashlib
 app = Flask(__name__)
 app.secret_key = "testing"
 app.config["TEMPLATES_AUTO_RELOAD"]=True
@@ -17,7 +18,14 @@ records = db.users
 print(records)
 @app.route("/")
 def index():
-    return render_template('home.html',messages={"loggedIn": True if "email" in session else False})
+    loggedIn = True if "email" in session else False
+    if loggedIn:
+        databases= db.databasesLinked.find({"email":session["email"]})
+        print(databases)
+        dbIDs = [[entry['databaseID'],entry['balance']] for entry in databases]
+        print(dbIDs)    
+        return render_template('home.html',messages={"loggedIn": True,"databases":dbIDs})
+    return render_template('home.html',messages={"loggedIn": False})
 
 # Pages
 @app.route("/showData")
@@ -57,6 +65,36 @@ def loginPage():
             message = 'Email not found'
             return render_template('loginPage.html', messages={"error":message,"loggedIn": True if "email" in session else False})
 
+
+def addDatabase(groupID,appID,trigID,emailID):
+    stringToHash = groupID + trigID + appID
+    hash_object = hashlib.sha1(stringToHash.encode())
+    hex_dig = hash_object.hexdigest()
+    print(hex_dig)
+    hashFound = db.databasesLinked.find_one({'databaseID':hex_dig})
+    print(hashFound)
+    if hashFound: 
+        return False
+    else:
+        db.databasesLinked.insert_one({'databaseID':hex_dig,'email':emailID,'balance':0})
+        return True
+
+@app.route('/addNewDatabase',methods=['GET','POST'])
+def addNewDatabase():
+    if request.method == 'GET':
+        if "email" not in session:
+            return redirect(url_for("index"))
+        return render_template('addNewDB.html',messages={"loggedIn": True if "email" in session else False})
+    else:
+        groupID = request.form.get('groupID')
+        appID = request.form.get('appID')
+        trigID = request.form.get('triggerID')
+        addedDB = addDatabase(groupID,appID,trigID,session["email"]) #Adds database into databasesLinked collection
+        if addedDB == False: 
+            message = 'This database trigger is already linked'
+            return render_template('addNewDB.html', messages={"error":message,"loggedIn": True if "email" in session else False})
+        else:
+            return render_template('addNewDB.html', messages={"error":"Added Succesfully. Check in Homepage","loggedIn": True if "email" in session else False})
 @app.route("/register", methods=['GET','POST'])
 def registerPage():
     if request.method == 'GET':
@@ -77,6 +115,13 @@ def registerPage():
             message = 'This email already exists in database'
             return render_template('register.html', messages={"error":message,"loggedIn": True if "email" in session else False})
         else:
+            groupID = request.form.get('groupID')
+            appID = request.form.get('appID')
+            trigID = request.form.get('triggerID')
+            addedDB = addDatabase(groupID,appID,trigID,emailID) #Adds database into databasesLinked collection
+            if addedDB == False: 
+                message = 'This database trigger is already linked'
+                return render_template('register.html', messages={"error":message,"loggedIn": True if "email" in session else False})
             hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
             user_input = {'email': emailID, 'password': hashed}
             records.insert_one(user_input)
@@ -85,7 +130,7 @@ def registerPage():
             new_email = user_data['email']
             print(user_data)
             session["email"] = emailID
-            return render_template('home.html', messages={"email":new_email,"loggedIn": True if "email" in session else False})
+            return redirect(url_for("index"))
         # return render_template('home.html',messages={})
 
 @app.route('/logout',methods=['GET','POST'])
